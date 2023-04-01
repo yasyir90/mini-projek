f01f8a72311f00156a2e26ac9ca59f560e78b495
yasyir90 <yasyirmasyal@gmail.com>
2023-04-01 09:51:43 +0700

    ganti karna eror

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const gallery = document.querySelector('.gallery');
-const jumbo = document.querySelector('.jumbo');
+// Example data
+const movies = [
+  {
+    title: "Movie Title 1",
+    description: "Movie Description 1",
+    poster: "https://via.placeholder.com/150x200",
+  },
+  {
+    title: "Movie Title 2",
+    description: "Movie Description 2",
+    poster: "https://via.placeholder.com/150x200",
+  },
+  {
+    title: "Movie Title 3",
+    description: "Movie Description 3",
+    poster: "https://via.placeholder.com/150x200",
+  },
+];
 
-gallery.addEventListener('click', function(e){
-  if(e.target.className == 'tumb'){
+// Add movies to the page
+const moviesContainer = document.querySelector(".movies");
 
-    jumbo.scr = e.target.src;
-  }
+movies.forEach((movie) => {
+  const movieDiv = document.createElement("div");
+  movieDiv.classList.add("movie");
+
+  const movieImg = document.createElement("img");
+  movieImg.src = movie.poster;
+  movieImg.alt = `${movie.title} Poster`;
+  movieDiv.appendChild(movieImg);
+
+  const movieTitle = document.createElement("h2");
+  movieTitle.textContent = movie.title;
+  movieDiv.appendChild(movieTitle);
+
+  const movieDescription = document.createElement("p");
+  movieDescription.textContent = movie.description;
+  movieDiv.appendChild(movieDescription);
+
+  moviesContainer.appendChild(movieDiv);
 });
